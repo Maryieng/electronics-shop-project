81cf20dc699e5ff8e5328e660891d74c6f8a54a9
Mary <kass.o@yandex.ru>
2023-11-20 14:49:40 +0300

    class ITEM implemented

--- src/item.py
+++ src/item.py
@@ -1,30 +1,22 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+    """Класс для представления товара в магазине. """
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+        """ Создание экземпляра класса item."""
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        """ Рассчитывает общую стоимость конкретного товара в магазине."""
+        return self.price * self.quantity
 
-        :return: Общая стоимость товара.
-        """
-        pass
 
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        """ Применяет установленную скидку для конкретного товара. """
+        self.price = self.price - self.price * self.pay_rate
