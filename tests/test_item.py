5d286012fdcee582de898a70b3b135f01a49cd86
Mary <kass.o@yandex.ru>
2023-11-23 17:33:46 +0300

    tests for new functions were written

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,3 +1,5 @@
+import unittest
+
 import pytest
 
 from src.item import Item
@@ -17,5 +19,48 @@ def test_apply_discount(item):
     assert item.price == 0.0
 
 
-def test_all(item):
-    assert item in Item.all
+def test_name(item):
+    item.name = "Смартфон"
+    assert item.name == "Смартфон"
+    item.name = "СуперСмартфон"
+    assert item.name == "СуперСмарт"
+    item.name = "Супер"
+    assert item.name == "Супер"
+
+
+class ItemTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sample_csv = 'sample.csv'
+
+    def tearDown(self):
+        Item.all = []
+
+    def test_instantiate_from_csv(self):
+        with open(self.sample_csv, 'w') as f:
+            f.write("name,price,quantity\n")
+            f.write("Item 1,10.99,5\n")
+            f.write("Item 2,5.99,3\n")
+
+        Item.instantiate_from_csv(self.sample_csv)
+        self.assertEqual(len(Item.all), 2)
+        self.assertEqual(Item.all[0].name, "Item 1")
+        self.assertEqual(Item.all[0].price, 10)
+        self.assertEqual(Item.all[0].quantity, 5)
+        self.assertEqual(Item.all[1].name, "Item 2")
+        self.assertEqual(Item.all[1].price, 5)
+        self.assertEqual(Item.all[1].quantity, 3)
+
+    def test_nonexistent_csv(self):
+        nonexistent_csv = 'sample.csv'
+        Item.instantiate_from_csv(nonexistent_csv)
+
+    def test_string_conversion(self):
+        with open(self.sample_csv, 'w') as f:
+            f.write("name,price,quantity\n")
+            f.write("Item 1,5,3\n")
+
+        Item.instantiate_from_csv(self.sample_csv)
+        self.assertEqual(Item.all[0].price, 5)
+        self.assertEqual(Item.all[0].quantity, 3)
+        self.assertIsInstance(Item.all[0].price, int)
+        self.assertIsInstance(Item.all[0].quantity, int)
