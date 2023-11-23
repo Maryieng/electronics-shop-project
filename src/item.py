9be2e9042e8f98a5fe75825f1f20a6634d52a0f6
Mary <kass.o@yandex.ru>
2023-11-23 17:33:26 +0300

    new functions added, access to variables configured

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import math
+
+
 class Item:
     """Класс для представления товара в магазине. """
     pay_rate = 1.0
@@ -5,10 +9,22 @@ class Item:
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """ Создание экземпляра класса item."""
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """ Возвращает наименование товара. """
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """ Устанавливает наименование товара. """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
 
     def calculate_total_price(self) -> float:
         """ Рассчитывает общую стоимость конкретного товара в магазине."""
@@ -17,3 +33,20 @@ class Item:
     def apply_discount(self) -> None:
         """ Применяет установленную скидку для конкретного товара. """
         self.price = self.price - self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        """инициализирует экземпляры класса Item данными из файла src/items.csv"""
+        with open(filename, 'r') as f:
+            reader = csv.DictReader(f, delimiter=',')
+            for line in reader:
+                name = line['name']
+                price = Item.string_to_number(line['price'])
+                quantity = int(line['quantity'])
+                item = cls(name, price, quantity)
+                Item.all.append(item)
+
+    @staticmethod
+    def string_to_number(string: str) -> float:
+        """Преобразование строки в число"""
+        return int(math.floor(float((string.replace(',', '.')))))
