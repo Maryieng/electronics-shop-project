9129d3a925903dcf8488eb573e17f546c0cdce08
Mary <kass.o@yandex.ru>
2023-12-05 10:30:05 +0300

    add new class Phone()

---
+++ src/phone.py
@@ -0,0 +1,22 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    def __repr__(self) -> str:
+        """ отображение информации об объекте класса в режиме отладки """
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_sim):
+        if number_sim <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        else:
+            self.__number_of_sim = number_sim
